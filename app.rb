d7c4391ca22d1e12726c3b1558b5304d30ba4946
Cristina <cristhinagv@gmail.com>
2022-02-08 14:57:14 +0000

    project set up

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+
+class BookmarkManager < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Bookmark Manager'
+  end
+
+  run! if app_file == $0
+end
