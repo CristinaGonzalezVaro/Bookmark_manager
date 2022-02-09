453ea3cfce051c44e9936c5ece06e6d895d244ef
Cristina <cristhinagv@gmail.com>
2022-02-09 16:43:01 +0000

    set up test environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,15 +15,25 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-ENV['RACK_ENV'] = 'test'
+
+
+ENV['ENVIRONMENT'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require_relative './setup_test_database'
 
 
 Capybara.app = BookmarkManager
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
+
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
